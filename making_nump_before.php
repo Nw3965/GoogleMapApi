dc81d5ed4bc20bdc4813624cf1b4742d5aa4e9d7
Nw3965 <hwrd231@gmail.com>
2016-11-04 19:25:01 +0900

    Add files via upload

---
+++ making_nump_before.php
@@ -0,0 +1,25 @@
+PHP,HTMLここに言語を入力
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<title>PHP数列</title>
+</head>
+<body>
+
+<p>PHP フィナボッチ数のテストです。</p>
+
+<p>
+<?php
+for($a = 1; $a+$b =$c){
+$c = $a;
+$a + $b <=70; ){
+echo $c;
+
+}
+?>
+</p>
+
+</body>
+</html> 
